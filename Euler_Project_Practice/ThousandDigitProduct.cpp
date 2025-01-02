b43712c2650375536629d30b590bc227145c7d96
Joaquin <joaquintola01@gmail.com>
2025-01-02 14:58:30 -0500

    starting the next problem, called ThousandDigitProduct - works with new text file added as well

---
+++ Euler_Project_Practice/ThousandDigitProduct.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main () {
+    /*
+    1. read from a csv or text file
+    2. store it into an array
+    3. go through array looking for the largest 13 product resulting in their product
+      3i. if there is a skip the section and move away *13 spaces* (check exact math later)
+    4. print result
+    */
+
+    return 0;
+}
+
+vector<int> thouDigitArray(){
+    /*
+    Here is where i will have to parse through a text file to generate the array
+    containing the 1000 digits
+    */
+    vector<int> array = {1};
+    return array;
+}
+
+int largestProduct(){
+    /*
+    Here is where i will go through the array and search for the largest product of
+    13 adjacent numbers
+    - include logic for cases with "0"
+    */
+    vector<int> thouArray = thouDigitArray();
+
+    
+    return 0;
+}
