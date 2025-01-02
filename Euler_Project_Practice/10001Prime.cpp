2e73a3ca50280b43e3aecb9720f2922bd425233e
Joaquin <joaquintola01@gmail.com>
2025-01-02 14:38:35 -0500

    added further notes to the 10001 problem

--- Euler_Project_Practice/10001Prime.cpp
+++ Euler_Project_Practice/10001Prime.cpp
@@ -50,3 +50,29 @@ int main() {
 
     return 0;
 }
+
+
+/*
+Visual representation:
+- Have a list size 8 
+
+[t][t][t][t][t][t][t][t][t][t]
+ 0  1  2  3  4  5  6  7  8  9
+
+- skip 0 and 1 as they are not prime numbers
+[f][f][t][t][t][t][t][t][t][t]
+ 0  1  2  3  4  5  6  7  8  9
+
+ - starting at 2 make every multiple false
+[f][f][t][t][f][t][f][t][f][t]
+ 0  1  2  3  4  5  6  7  8  9
+
+ - move to 3 and start at 3x3 (3x2) is already taken care of and will be the case for every multiple where i<i*i
+[f][f][t][t][f][t][f][t][f][f]
+ 0  1  2  3  4  5  6  7  8  9
+
+ - this iteration only goes on while i < sqrt(n) where n is 9 in this case
+ - after 3, the remaining numbers are prime numbers:
+
+2, 3, 5, 7
+*/
