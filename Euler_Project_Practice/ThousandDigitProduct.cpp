bf2caa72798b3e4b1df3ee11cccdbfd37472c637
Joaquin <joaquintola01@gmail.com>
2025-02-06 12:28:37 -0500

    more development on ThousandDigitProduct

--- Euler_Project_Practice/ThousandDigitProduct.cpp
+++ Euler_Project_Practice/ThousandDigitProduct.cpp
@@ -4,38 +4,28 @@
 #include <fstream>
 using namespace std;
 
-int main () {
-    ifstream inputFile("ThousandDigitNumber.txt");
-    if (!inputFile.is_open()) {
-        cerr << "Error: Could not open file!" << endl;
-        return 1;
-    }
-
-    string line;
-    while (getline(inputFile, line)){
-        cout << line << endl;
-    }
-        
-    inputFile.close();
-    return 0;
-    /*
-    1. read from a csv or text file
-    2. store it into an array
-    3. go through array looking for the largest 13 product resulting in their product
-      3i. if there is a skip the section and move away *13 spaces* (check exact math later)
-    4. print result
-    */
-
-    return 0;
-}
-
 vector<int> thouDigitArray(){
     /*
     Here is where i will have to parse through a text file to generate the array
     containing the 1000 digits
     */
-    vector<int> array = {1};
-    return array;
+    ifstream inputFile("ThousandDigitNumber.txt");
+    if (!inputFile.is_open()) {
+        cerr << "Error: Could not open file!" << endl;
+        return {};
+    }
+
+    vector<int> digits;
+    char digit;
+
+    while (inputFile.get(digit)){
+        if (isdigit(digit)){
+            digits.push_back(digit - '0');
+        }
+    }
+        
+    inputFile.close();
+    return digits;
 }
 
 int largestProduct(){
@@ -47,5 +37,27 @@ int largestProduct(){
     vector<int> thouArray = thouDigitArray();
 
     
+    return 0;
+}
+
+int main () {
+    /*
+    1. read from a csv or text file
+    2. store it into an array
+    3. go through array looking for the largest 13 product resulting in their product
+      3i. if there is a skip the section and move away *13 spaces* (check exact math later)
+    4. print result
+    */
+
+    vector<int> digits = thouDigitArray();
+
+    if (digits.empty()){
+        cerr << "Failed to read digits from the file" << endl;
+    }
+
+    for (size_t i = 0; i < 5 && i < digits.size(); i++){
+        cout << digits[i] << " ";
+    }
+
     return 0;
 }
